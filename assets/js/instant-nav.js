1aeb8fb2c54f04d5e520e8de98f4a5859c5c1461
Nicolas Rio <nicolas.rio42@gmail.com>
2022-03-09 12:02:31 +0100

    logging links href

--- assets/js/instant-nav.js
+++ assets/js/instant-nav.js
@@ -1,1 +1,12 @@
-console.log("instant nav works !")
+console.log("instant nav works !");
+
+const links = document.querySelectorAll("a");
+
+[...links].forEach((link) => {
+  link.onclick = (e) => {
+    e.preventDefault();
+    fetch(link.href).then((response) => {
+      console.log(response);
+    });
+  };
+});
