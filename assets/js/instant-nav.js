05ade0a2d4bfc92c9c1ab223bccbfc7bf1ec0ec3
Nicolas Rio <nicolas.rio42@gmail.com>
2022-03-09 12:09:03 +0100

    parsing response

--- assets/js/instant-nav.js
+++ assets/js/instant-nav.js
@@ -5,8 +5,12 @@ const links = document.querySelectorAll("a");
 [...links].forEach((link) => {
   link.onclick = (e) => {
     e.preventDefault();
-    fetch(link.href).then((response) => {
-      console.log(response);
-    });
+    fetch(link.href)
+      .then((response) => response.text())
+      .then((res) => {
+        const parser = new DOMParser();
+        const document = parser.parseFromString(res, "text/html");
+        console.log(document);
+      });
   };
 });
