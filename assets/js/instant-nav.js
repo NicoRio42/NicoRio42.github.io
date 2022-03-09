345fd24045fdc743763461858a94fcdff0296ef3
Nicolas Rio <nicolas.rio42@gmail.com>
2022-03-09 12:26:05 +0100

    normal behavior for external links

--- assets/js/instant-nav.js
+++ assets/js/instant-nav.js
@@ -3,15 +3,17 @@ console.log("instant nav works !");
 const links = document.querySelectorAll("a");
 
 [...links].forEach((link) => {
-  link.onclick = (e) => {
-    e.preventDefault();
-    fetch(link.href)
-      .then((response) => response.text())
-      .then((res) => {
-        const parser = new DOMParser();
-        const linkDoc = parser.parseFromString(res, "text/html");
-        const main = document.getElementById("main");
-        main.innerHTML = linkDoc.getElementById("main").innerHTML;
-      });
-  };
+  if (document.location.origin === link.origin) {
+    link.onclick = (e) => {
+      e.preventDefault();
+      fetch(link.href)
+        .then((response) => response.text())
+        .then((res) => {
+          const parser = new DOMParser();
+          const linkDoc = parser.parseFromString(res, "text/html");
+          const main = document.getElementById("main");
+          main.innerHTML = linkDoc.getElementById("main").innerHTML;
+        });
+    };
+  }
 });
